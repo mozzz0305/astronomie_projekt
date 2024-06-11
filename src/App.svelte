2f6be7c9672c7f6b8abd603e3115259527c9730b
mozzz0305 <136460090+mozzz0305@users.noreply.github.com>
2024-06-11 18:24:13 +0200

    evn startklar gemacht

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  const apiKey = import.meta.env.VITE_TEST;
+  console.log("Thats the key: " + apiKey); // This will print your API key from the .env file
+  console.log('Hello world');
+</script>
